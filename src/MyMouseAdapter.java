1bdd75f7364ae0852a31d378ea3ef240e4ca28c9
gabriel_ubuntu <gabriel_ubuntu@Gabriels-Laptop>
2016-10-09 14:03:39 -0400

    Implemented a method to count adjacent mines to each uncovered cell.

--- src/MyMouseAdapter.java
+++ src/MyMouseAdapter.java
@@ -87,76 +87,70 @@ public class MyMouseAdapter extends MouseAdapter {
 			
 			myPanel.x = x;
 			myPanel.y = y;
-			
+
 			int gridX = myPanel.getGridX(x, y);
 			int gridY = myPanel.getGridY(x, y);
-			
+
 			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
 				//Had pressed outside
 				//Do nothing
-			} else {
-				if ((gridX == -1) || (gridY == -1)) {
-					//Is releasing outside
-					//Do nothing
-				} else {
-					if ((myPanel.mouseDownGridX == gridX) && (myPanel.mouseDownGridY == gridY)) {
-						
-						//Released the mouse button on the same cell where it was pressed
-						if ((gridX == 0) || (gridY == 0)) {
-							//On the left column and on the top row... do nothing
-						} else {
-							//On the grid other than on the left column and on the top row:
-							Color newColor = Color.cyan;
-							
-							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
-							myPanel.repaint();
-						}
-					}
+			} else  if ((gridX == -1) || (gridY == -1)) {
+				//Is releasing outside
+				//Do nothing
+			} else if ((myPanel.mouseDownGridX == gridX) && (myPanel.mouseDownGridY == gridY)) {
+				//Released the mouse button on the same cell where it was pressed
+				if (myPanel.Mines[myPanel.mouseDownGridX][myPanel.mouseDownGridY] == 0){
+					myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = Color.BLACK;
+				}else{
+					int mines = MineCounter(myPanel.mouseDownGridX, myPanel.mouseDownGridY, myPanel);
+					
+					System.out.println(mines);
+					Color newColor = Color.LIGHT_GRAY;
+					
+					myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
+					myPanel.repaint();
 				}
 			}
+
 			myPanel.repaint();
 			break;
 		case 3:
 			c = e.getComponent();
-			
+
 			while (!(c instanceof JFrame)) {
 				c = c.getParent();
 				if (c == null) {
 					return;
 				}
 			}
-			
+
 			myFrame = (JFrame)c;
 			myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
 			myInsets = myFrame.getInsets();
-			
+
 			x1 = myInsets.left;
 			y1 = myInsets.top;
-			
+
 			e.translatePoint(-x1, -y1);
-			
+
 			x = e.getX();
 			y = e.getY();
-			
+
 			myPanel.x = x;
 			myPanel.y = y;
-			
+
 			gridX = myPanel.getGridX(x, y);
 			gridY = myPanel.getGridY(x, y);
-			
+
 			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
 				//Had pressed outside
 				//Do nothing
-			} else {
-				if ((gridX == -1) || (gridY == -1)) {
-					//Is releasing outside
-					//Do nothing
-				} else {
-					if ((myPanel.mouseDownGridX == gridX) && (myPanel.mouseDownGridY == gridY)) {
-						myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = Color.RED;
-					}
-				}	
-			}
+			} else if ((gridX == -1) || (gridY == -1)) {
+				//Is releasing outside
+				//Do nothing
+			} else if ((myPanel.mouseDownGridX == gridX) && (myPanel.mouseDownGridY == gridY)){
+				myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = Color.RED;
+			}	
 			myPanel.repaint();
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
@@ -164,4 +158,37 @@ public class MyMouseAdapter extends MouseAdapter {
 			break;
 		}
 	}
+
+	public int MineCounter(int mouseDownX, int mouseDownY, MyPanel myPanel){
+		int mineCounter = 0;
+		for (int i = mouseDownY-1; i <= mouseDownY+1; i++){
+			for (int j = mouseDownX-1; j <= mouseDownX+1;j++){
+				try {
+					if (myPanel.Mines[i][j] == 0){
+						mineCounter++;
+					}
+				} catch (Exception e){}
+			}
+		}
+		return mineCounter; 
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
